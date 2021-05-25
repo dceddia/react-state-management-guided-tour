9ba444bc08bba516e6afe96994541e8dcd29d5ca
Dave Ceddia <dave@daveceddia.com>
2021-05-25 18:09:58 -0400

    Before XState

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
-import React, { useState } from 'react';
-import { createMachine } from 'xstate';
-import { useMachine } from '@xstate/react';
-
-const trafficLight = createMachine({
-  id: 'light',
-  initial: 'green',
-  states: {
-    green: {
-      on: {
-        TIMER: 'yellow'
-      }
-    },
-    yellow: {
-      on: {
-        TIMER: 'red'
-      }
-    },
-    red: {
-      on: {
-        TIMER: 'green'
-      }
-    }
-  }
-})
+import React from 'react';
 
 const App = () => {
-  const [state, send] = useMachine(trafficLight);
-
   return (
-    <div>
-      <p>Traffic light is: {state.value}</p>
-      <button onClick={() => send('TIMER')}>Next</button>
-      <pre>
-        {JSON.stringify(state, null, 2)}
-      </pre>
+    <div className="mx-12 my-5">
+      starting over
     </div>
   );
 };
