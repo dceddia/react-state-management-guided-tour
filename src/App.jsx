20b7812d28a9900ef34982768f27204ed0f739fb
Dave Ceddia <dave@daveceddia.com>
2021-06-01 23:16:51 -0400

    Xstate

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import Todos from './Todos';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import React, { useState } from 'react';
+import { createMachine } from 'xstate';
+import { useMachine } from '@xstate/react';
 
-// One long-lived QueryClient
-const client = new QueryClient();
+const trafficLight = createMachine({
+  id: 'light',
+  initial: 'green',
+  states: {
+    green: {
+      on: {
+        TIMER: 'yellow',
+      },
+    },
+    yellow: {
+      on: {
+        TIMER: 'red',
+      },
+    },
+    red: {
+      on: {
+        TIMER: 'green',
+      },
+    },
+  },
+});
+
+const colors = {
+  red: 'bg-red-500',
+  yellow: 'bg-yellow-400',
+  green: 'bg-green-500',
+};
 
 const App = () => {
+  const [state, send] = useMachine(trafficLight);
   return (
-    <QueryClientProvider client={client}>
-      <main>
-        <Todos />
-      </main>
-    </QueryClientProvider>
+    <div className="m-5">
+      <p className="mb-5 flex items-center">
+        Traffic light is:
+        <span
+          className={`${
+            colors[state.value]
+          } ml-2 mr-1 w-4 h-4 inline-block rounded-full`}
+        ></span>
+        {state.value}
+      </p>
+      <button onClick={() => send('TIMER')} className="mb-5">
+        Next
+      </button>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </div>
   );
 };
 
